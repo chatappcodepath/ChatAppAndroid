d0a6fdd92c7236eb862380c3b092114bf4ac9e74
dsatija <gurucharan.shiriyaar@gmail.com>
2016-12-01 23:50:43 -0800

    Add on View

--- app/src/main/java/com/google/firebase/codelab/friendlychat/activities/IndividualChatActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/activities/IndividualChatActivity.java
@@ -21,10 +21,14 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -36,6 +40,7 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -43,9 +48,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.codelab.friendlychat.R;
 import com.google.firebase.codelab.friendlychat.adapters.MessageListAdapter;
+import com.google.firebase.codelab.friendlychat.adapters.ViewPagerAdapter;
 import com.google.firebase.codelab.friendlychat.chatAddons.MessageViewHolder;
 import com.google.firebase.codelab.friendlychat.chatAddons.movie.fragments.MovieFragment;
 import com.google.firebase.codelab.friendlychat.chatAddons.movie.fragments.TrailerFragment;
+import com.google.firebase.codelab.friendlychat.chatAddons.tictactoe.fragments.TicTacToeFragment;
 import com.google.firebase.codelab.friendlychat.models.FriendlyMessage;
 import com.google.firebase.codelab.friendlychat.utilities.AddonsProtocols;
 import com.google.firebase.codelab.friendlychat.utilities.ChatApplication;
@@ -54,7 +61,11 @@ import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.google.firebase.codelab.friendlychat.chatAddons.movie.helpers.NestedScrollableViewHelper;
 
+import static android.view.View.VISIBLE;
+import static android.widget.Toast.LENGTH_SHORT;
 import static com.google.firebase.codelab.friendlychat.utilities.FirebaseClient.MESSAGES_FOR_GROUP_NODE;
 
 public class IndividualChatActivity extends AppCompatActivity
@@ -82,6 +93,16 @@ public class IndividualChatActivity extends AppCompatActivity
     private LinearLayout linearLayout;
     private LinearLayout linearLayout_fragment;
     private FrameLayout flMovieFragment;
+
+    private Toolbar toolbar1;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private SlidingUpPanelLayout slidingLayout;
+    private int[] tabIcons = {
+            R.drawable.ic_movieslogo,
+            R.drawable.ic_tictactoe,
+
+    };
 
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
@@ -215,9 +236,23 @@ public class IndividualChatActivity extends AppCompatActivity
         });
     }
 
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+    }
+
 
     @Override
     public void onPreviewFragmentInteraction() {
+    }
+
+    //added by disha
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MovieFragment(), "Movies");
+        adapter.addFragment(new TicTacToeFragment(), "TicTacToe");
+        viewPager.setAdapter(adapter);
     }
 
 
@@ -270,37 +305,111 @@ public class IndividualChatActivity extends AppCompatActivity
     }
 
     public void onAddTrailor(View view) {
-        //reset linear layout
-        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) linearLayout.getLayoutParams();
-        lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        lp.addRule(RelativeLayout.CENTER_VERTICAL);
-        lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,0);
-       // lp.removeRule(layout_alignParentBottom);
-        linearLayout.setLayoutParams(lp);
-        //reset fragment layout
-        RelativeLayout.LayoutParams lpf = (RelativeLayout.LayoutParams) linearLayout_fragment.getLayoutParams();
-        lpf.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
-        lpf.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        linearLayout_fragment.setLayoutParams(lpf);
+        linearLayout.setVisibility(View.INVISIBLE);
+        setAddonView();
+        animateAddonView();
+        setSlidingPanel();
+        if(slidingLayout != null){
+            Toast.makeText(this,slidingLayout.getPanelState().toString(),LENGTH_SHORT).show();
 
+        }
+
+
+        /*
         // Begin the transaction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 // Replace the contents of the container with the new fragment
         ft.replace(R.id.flMovieFragment, new MovieFragment(),"movies");
 // Complete the changes added above
         ft.commit();
+        */
     }
 
+    private void setSlidingPanel() {
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setVisibility(VISIBLE);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+        slidingLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+        NestedScrollableViewHelper nsv = new NestedScrollableViewHelper();
+        nsv.setScrollableView(findViewById(R.id.gvTrailor));
+
+        slidingLayout.setAnchorPoint(0.5f);
+        slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.ANCHORED);
+        slidingLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+                linearLayout.setVisibility(VISIBLE);
+            }
+        });
+        slidingLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+
+            @Override
+            public void onPanelSlide(View view, float v) {
+
+                //Toast.makeText(getApplicationContext(),"sliding + "+ v, LENGTH_SHORT).show();
+
+
+
+            }
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState,
+                                            SlidingUpPanelLayout.PanelState newState) {
+            // Toast.makeText(getApplicationContext(),newState.toString(),Toast.LENGTH_SHORT).show();
+                if(newState == SlidingUpPanelLayout.PanelState.COLLAPSED ||
+                        newState == SlidingUpPanelLayout.PanelState.HIDDEN){
+                    linearLayout.setVisibility(VISIBLE);
+                }
+
+
+            }
+
+        });
+
+
+
+    }
+
+    private void animateAddonView() {
+        viewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(View page, float position) {
+                /**SCALING TRANSFORMATION**/
+                final float normalizedposition = Math.abs(Math.abs(position) - 1);
+                page.setScaleX(normalizedposition / 2 + 0.5f);
+                page.setScaleY(normalizedposition / 2 + 0.5f);
+                /**ROATAION TRANSFORMATION**/
+                page.setRotationY(position * -30);
+            }
+        });
+
+    }
+
+    private void setAddonView() {
+        AppBarLayout alAddon = (AppBarLayout) findViewById(R.id.alAddon);
+        alAddon.setVisibility(VISIBLE);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+    }
+
+
     public void CancelTrailers(View view) {
+        slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+        /*
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.remove(getSupportFragmentManager().findFragmentByTag("movies"));
+
+        ft.remove(getSupportFragmentManager().findFragmentById(R.id.flMovieFragment));
         ft.commit();
 
         RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) linearLayout.getLayoutParams();
         lp.addRule(RelativeLayout.CENTER_HORIZONTAL,0);
         lp.addRule(RelativeLayout.CENTER_VERTICAL,0);
         lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        linearLayout.setLayoutParams(lp);
+        linearLayout.setLayoutParams(lp);*/
     }
 
     public void CancelPreview(View view){
